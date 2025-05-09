93bd32466e39b8659a36f4f95550d9222e7d48e3
Benjamin Chen <bc880321@gmail.com>
2025-05-08 21:06:45 -0700

    Initial Commit

---
+++ Candy Pop/Candy_PopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Candy_PopApp.swift
+//  Candy Pop
+//
+//  Created by Benjamin Chen on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Candy_PopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
